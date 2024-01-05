c2b61593bfd843da138d9e532fc9c2d4d8bf06f2
matheusbudin <matheusbudin@hotmail.com>
2024-01-05 19:36:51 -0300

    feat/ added staticmethod and agregation

--- shopping_cart_system/shopping/shopping_cart.py
+++ shopping_cart_system/shopping/shopping_cart.py
@@ -1,9 +1,16 @@
 class ShoppingCart:
-    def __init__(self):
+    def __init__(self, discount=None):
         self.items = []
+        self.discount = discount  # Aggregation of Discount class
 
     def add_item(self, item):
-        self.items.append(item)
+        if ShoppingCart._validate_item_name(item):
+            self.items.append(item)
 
     def total_cost(self):
-        return sum(item.get_price() for item in self.items)
+        total = sum(item.get_price() for item in self.items)
+        return self.discount.apply_discount(total) if self.discount else total
+
+    @staticmethod
+    def _validate_item_name(item):
+        return isinstance(item, Item) and len(item.get_price()) > 0
