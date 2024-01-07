95b535180772427dbdf1fd9786769f566920a9b0
matheusbudin <matheusbudin@hotmail.com>
2024-01-06 21:26:11 -0300

    feat/ unit test for food product

---
+++ product_management_system/tests/test_food_product.py
@@ -0,0 +1,10 @@
+import unittest
+from product_management.food_product import FoodProduct
+
+class TestFoodProduct(unittest.TestCase):
+    def test_display_info(self):
+        product = FoodProduct("Apple", 1)
+        self.assertEqual(product.display_info(), "Food Product - Apple, Base Price: 1")
+
+if __name__ == '__main__':
+    unittest.main()
